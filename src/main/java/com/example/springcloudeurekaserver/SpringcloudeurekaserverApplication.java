52549fe796e9b862ef2f90f244a3465ddbe4fdbd
wangs <993752649@qq.com>
2019-12-09 22:13:37 +0800

    首次提交

---
+++ src/main/java/com/example/springcloudeurekaserver/SpringcloudeurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudeurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudeurekaserverApplication.class, args);
+	}
+
+}
